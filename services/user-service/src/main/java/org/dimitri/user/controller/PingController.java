deac2dcb8f3877f264885a1ee972db8475c406f2
Dimitri Chelloul <chellouldimitri@aol.fr>
2026-01-29 07:58:00 +0100

    premier push aplli java sprigboot

---
+++ services/user-service/src/main/java/org/dimitri/user/controller/PingController.java
@@ -0,0 +1,13 @@
+package org.dimitri.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "user-service: pong";
+    }
+}
